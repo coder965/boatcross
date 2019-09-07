8d72037a9fff975e946e9bb647675e166dae6410
Zeng <kelylongyun@163.com>
2019-09-07 16:15:13 +0800

    项目初始提交

---
+++ boatcross-server/src/main/java/com/kelystor/boatcross/BoatcrossApplication.java
@@ -0,0 +1,13 @@
+package com.kelystor.boatcross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatcrossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatcrossApplication.class, args);
+	}
+
+}
